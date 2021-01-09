db2a321fa3f5f262d450bf6a0509c65a6048c463
Mupa1 <mupasmail@gmail.com>
2021-01-09 20:34:53 +0100

    add functionality to log_out a user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::SessionsController < Devise::SessionsController
   before_action :sign_in_params, only: :create
   before_action :load_user, only: :create
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
 
   def create
     if @user.valid_password?(sign_in_params[:password])
@@ -9,6 +11,12 @@ class Api::V1::SessionsController < Devise::SessionsController
     else
       json_response 'Unauthorized', false, {}, :unauthorized
     end
+  end
+
+  def destroy
+    sign_out @user
+    @user.generate_new_authentication_token
+    json_response 'Log out Successfully', true, {}, :ok
   end
 
   private
@@ -20,6 +28,11 @@ class Api::V1::SessionsController < Devise::SessionsController
   def load_user
     @user = User.find_for_database_authentication(email: sign_in_params[:email])
 
-    @user || json_response('Cannot get User', false, {}, :failure)
+    @user || json_response('Cannot get User', false, {}, :not_found)
+  end
+
+  def valid_token
+    @user = User.find_by authentication_token: request.headers['AUTH-TOKEN']
+    @user || json_response('Invalid Token', false, {}, :not_found)
   end
 end
